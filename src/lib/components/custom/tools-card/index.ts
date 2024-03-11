3ccee46d2f5c30b97c49542e7cc9c3072e232db1
Raihan Adam <hallwack.id@gmail.com>
2024-03-11 23:43:46 +0700

    feat: create tools-card component

---
+++ src/lib/components/custom/tools-card/index.ts
@@ -0,0 +1,32 @@
+export const stackList = [
+	{ href: "https://react.dev", title: "React", desc: "For building UI" },
+	{
+		href: "https://nextjs.org",
+		title: "NextJS",
+		desc: "Meta Framework for building web"
+	},
+	{ href: "https://laravel.com", title: "Laravel", desc: "For building web but in PHP" },
+	{
+		href: "https://expressjs.com",
+		title: "ExpressJS",
+		desc: "For building web backend but in JavaScript"
+	},
+	{
+		href: "https://tailwindcss.com",
+		title: "TailwindCSS",
+		desc: "For styling web"
+	}
+];
+
+export const toolsList = [
+	{ href: "https://neovim.io", title: "Neovim", desc: "The best text editor" },
+	{ href: "https://code.visualstudio.com", title: "VSCode", desc: "For text editing" },
+	{ href: "https://git-scm.com", title: "Git", desc: "Version control system" },
+	{ href: "https://www.zsh.org", title: "Zsh", desc: "Terminal shell" },
+	{ href: "https://sw.kovidgoyal.net/kitty/", title: "Kitty", desc: "Terminal emulator" },
+	{
+		href: "https://github.com/jesseduffield/lazygit",
+		title: "Lazygit",
+		desc: "Simple TUI for git commands"
+	}
+];
---
+++ src/lib/components/custom/tools-card/tools-card.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { ArrowUpRight } from "lucide-svelte";
+
+	export let href: string;
+	export let title: string;
+	export let desc: string;
+</script>
+
+<a
+	{href}
+	target="_blank"
+	rel="noreferrer"
+	class="flex w-full justify-between rounded-lg border px-4 py-3 shadow-lg shadow-white transition-shadow duration-300 hover:shadow-black/10 dark:shadow-black dark:hover:shadow-white/10"
+>
+	<div class="flex flex-col gap-2">
+		<p class="text-xl font-semibold">{title}</p>
+		<span class="text-sm font-normal">{desc}</span>
+	</div>
+	<ArrowUpRight size={24} />
+</a>
