3608db3ad5a9b294d809d139e562c66d5833e371
Raihan Adam <hallwack.id@gmail.com>
2024-03-11 22:41:54 +0700

    feat: create contact-links component

---
+++ src/lib/components/custom/contact-links/contact-links.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { buttonVariants } from "@/components/ui/button";
+	import { contactLinks } from "./index";
+</script>
+
+<div class="flex gap-4 self-end">
+	{#each contactLinks as link}
+		<a
+			class={buttonVariants({ variant: "ghost", size: "icon" })}
+			href={link.href}
+			target="_blank"
+			rel="noreferrer"
+		>
+			<svelte:component this={link.component} />
+		</a>
+	{/each}
+</div>
---
+++ src/lib/components/custom/contact-links/index.ts
@@ -0,0 +1,9 @@
+import { LinkedinLogo, FigmaLogo, GithubLogo, InstagramLogo, EnvelopeClosed } from "svelte-radix";
+
+export const contactLinks = [
+	{ component: LinkedinLogo, href: "https://www.linkedin.com/in/raihan-adam-4011ba1b9" },
+	{ component: FigmaLogo, href: "https://www.figma.com/@raihanadam" },
+	{ component: GithubLogo, href: "https://github.com/hallwack" },
+	{ component: InstagramLogo, href: "https://instagram.com/raihanadamstagram" },
+	{ component: EnvelopeClosed, href: "mailto:hallwack.id@gmail.com" }
+];
