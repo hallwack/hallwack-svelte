6a9da98fa92890f9facad92bdec46317c5d4fb20
Raihan Adam <hallwack.id@gmail.com>
2024-03-11 20:14:41 +0700

    feat: add page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import Photo from "$lib/assets/ava.webp";
+</script>
+
+<div class="flex flex-col gap-4 text-justify text-lg leading-relaxed">
+	<h1 class="text-2xl font-medium tracking-wide">Hi, I'm Raihan Adam 👋</h1>
+	<div class="flex flex-col gap-8">
+		<p>
+			I'm a 10th semester student majoring in Informatics Engineering at UIN Sunan Gunung Djati
+			Bandung. Over the past three years, I have been working as a junior web developer at a startup
+			in Bandung.
+		</p>
+
+		<div class="flex items-center justify-center">
+			<img src={Photo} alt="Raihan Adam" class="w-72 rounded-lg object-cover" />
+		</div>
+
+		<p>
+			Throughout my career journey, I have successfully completed various exciting web projects that
+			have had a positive impact. I believe that technology can provide innovative solutions to
+			everyday challenges, which inspires me to continuously develop my skills and knowledge in this
+			field.
+		</p>
+	</div>
+
+</div>
