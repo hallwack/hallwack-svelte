23b8af0ebf6ffcf88f49361c1d5c03aa556f4acc
Raihan Adam <hallwack.id@gmail.com>
2024-03-11 20:14:31 +0700

    feat: add layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import "../app.pcss";
+	import Navbar from "$lib/components/custom/navbar/navbar.svelte";
+	import "@fontsource-variable/plus-jakarta-sans";
+</script>
+
+<div class="container mx-auto min-h-screen max-w-4xl antialiased">
+	<Navbar />
+	<slot />
+</div>
+
+<style>
+	:global(body) {
+		font-family: "Plus Jakarta Sans Variable", sans-serif;
+	}
+</style>
+
