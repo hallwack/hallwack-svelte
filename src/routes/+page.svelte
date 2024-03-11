fca3ac5a618d431482d1b229ea95e86fe609b1bc
Raihan Adam <hallwack.id@gmail.com>
2024-03-11 23:44:32 +0700

    fix: fixing index page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,28 @@
 <script lang="ts">
+	import { fade } from "svelte/transition";
 	import Photo from "$lib/assets/ava.webp";
+	import ContactLinks from "@/components/custom/contact-links/contact-links.svelte";
 </script>
 
-<div class="flex flex-col gap-4 text-justify text-lg leading-relaxed">
-	<h1 class="text-2xl font-medium tracking-wide">Hi, I'm Raihan Adam 👋</h1>
-	<div class="flex flex-col gap-8">
-		<p>
-			I'm a 10th semester student majoring in Informatics Engineering at UIN Sunan Gunung Djati
-			Bandung. Over the past three years, I have been working as a junior web developer at a startup
-			in Bandung.
-		</p>
+<div in:fade class="flex flex-col gap-8 text-justify text-lg font-medium leading-relaxed">
+	<h1 class="text-2xl font-semibold tracking-wide">Hi, I'm Raihan Adam 👋</h1>
 
-		<div class="flex items-center justify-center">
-			<img src={Photo} alt="Raihan Adam" class="w-72 rounded-lg object-cover" />
-		</div>
+	<p>
+		I'm a 10th semester student majoring in Informatics Engineering at UIN Sunan Gunung Djati
+		Bandung. Over the past four years, I have been working as a junior web developer at a startup in
+		Bandung.
+	</p>
 
-		<p>
-			Throughout my career journey, I have successfully completed various exciting web projects that
-			have had a positive impact. I believe that technology can provide innovative solutions to
-			everyday challenges, which inspires me to continuously develop my skills and knowledge in this
-			field.
-		</p>
+	<div class="flex items-center justify-center">
+		<img src={Photo} alt="Raihan Adam" class="w-72 rounded-lg object-cover" />
 	</div>
 
+	<p>
+		Throughout my career journey, I have successfully completed various exciting web projects that
+		have had a positive impact. I believe that technology can provide innovative solutions to
+		everyday challenges, which inspires me to continuously develop my skills and knowledge in this
+		field.
+	</p>
+
+	<ContactLinks />
 </div>
