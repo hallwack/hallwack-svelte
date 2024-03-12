f11ab8ef0472662528b11078f4e170b402ef5fc3
Raihan Adam <hallwack.id@gmail.com>
2024-03-12 16:12:12 +0700

    feat: add blog page

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { fade } from "svelte/transition";
+</script>
+
+<div
+	in:fade
+	class="my-8 flex flex-col gap-8 text-justify text-base font-medium leading-relaxed sm:text-lg"
+>
+	<h1 class="text-2xl font-bold tracking-wide">blog 📚</h1>
+
+	<p>Coming Soon!</p>
+</div>
