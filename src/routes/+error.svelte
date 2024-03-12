459d010bffa01a84abc193df2a9984e07c6db1cf
Raihan Adam <hallwack.id@gmail.com>
2024-03-12 18:54:42 +0700

    feat: create error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+	import { fade } from "svelte/transition";
+
+	$: console.error($page.error);
+</script>
+
+<div
+	in:fade
+	class="my-8 flex flex-col gap-8 text-justify text-base font-medium leading-relaxed sm:text-lg"
+>
+	<h1 class="text-2xl font-bold tracking-wide">{$page.status}: {$page.error?.message} ❌</h1>
+</div>
