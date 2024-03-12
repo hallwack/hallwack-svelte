73ec237a6bb22cb35bf222a1ebc84d78ecc700e7
Raihan Adam <hallwack.id@gmail.com>
2024-03-12 15:50:26 +0700

    feat: create works-card component

---
+++ src/lib/components/custom/works-card/index.ts
@@ -0,0 +1,49 @@
+import Hairnerds from "$lib/assets/hairnerds.png";
+import HairnerdsAcademy from "$lib/assets/hairnerds-academy.png";
+import Tanoseal from "$lib/assets/tanoseal.png";
+import Hellowork from "$lib/assets/hellowork.png";
+import EasyrentBali from "$lib/assets/easyrent-bali.png";
+
+export const worksList = [
+	{
+		title: "Hairnerds",
+		desc: "In this project, I've just changed all of this website's UI, especially in home page.",
+		href: "https://hairnerds.id/",
+		website: "hairnerds.id",
+		src: Hairnerds,
+		alt: "Hairnerds Website"
+	},
+	{
+		title: "Hairnerds Academy",
+		desc: "In this project, I've been fixing and developing some features like course registration, transactions, and administrator user interfaces.",
+		href: "https://academy.hairnerds.id/",
+		website: "academy.hairnerds.id",
+		src: HairnerdsAcademy,
+		alt: "Hairnerds Academy Website"
+	},
+	{
+		title: "Tanoseal E-Commerce",
+		desc: "Me and my team developing e-commerce applications for company in Japan. We using ReactJS and NextJs to building frontend application, and ExpressJs to build the backend application.",
+		href: "https://tanoseal.com/",
+		website: "tanoseal.com",
+		src: Tanoseal,
+		alt: "Tanoseal Website"
+	},
+	{
+		title: "Hellowork Education",
+		desc: "A website for applying courses. I've been working on building user interface and logic for backend business using Laravel and ReactJS.",
+		href: "https://edu.hellowork-asia.com/",
+		website: "edu.hellowork-asia.com",
+		src: Hellowork,
+		alt: "Hellowork Education Website"
+	},
+	{
+		title: "Easyrent Bali",
+		desc: "Me and my team developing car rent website applications for company in Bali. We using ReactJS and NextJs to building frontend application, and ExpressJs to build the backend application.",
+		href: "https://easyrentbali.com/",
+		website: "easyrentbali.com",
+		src: EasyrentBali,
+		alt: "Easyrent Bali Website",
+		isLastCard: true
+	}
+];
---
+++ src/lib/components/custom/works-card/works-card.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import Button from "@/components/ui/button/button.svelte";
+	import { cn } from "@/utils.js";
+	import { ArrowUpRight } from "lucide-svelte";
+
+	export let title: string;
+	export let desc: string;
+	export let href: string;
+	export let website: string;
+	export let src: string;
+	export let alt: string;
+	export let isLastCard = false;
+</script>
+
+<div class={cn("space-y-6 pb-8", !isLastCard && "border-b border-b-primary/20")}>
+	<div class="space-y-2">
+		<h3 class="text-2xl font-bold">{title}</h3>
+		<p>{desc}</p>
+	</div>
+	<div class="flex items-center justify-center">
+		<img {src} {alt} class="w-full rounded-lg object-cover" />
+	</div>
+	<Button {href} target="_blank" rel="noreferrer" class="gap-2 rounded-full text-base" variant="outline"
+		>{website} <ArrowUpRight /></Button
+	>
+</div>
