2e3797ef403953c744dfe2d4c067f79f20ac3582
Raihan Adam <hallwack.id@gmail.com>
2024-03-14 23:15:22 +0700

    feat: add blog slug page

---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { PageData } from "./$types";
+	import { formatDate } from "@/utils.js";
+
+	export let data: PageData;
+	const { title, Content, date } = data;
+</script>
+
+<article>
+	<hgroup class="my-8 flex flex-col gap-2">
+		<h1 class="text-5xl font-extrabold">{title}</h1>
+		<span class="text-sm font-medium text-slate-600 dark:text-slate-400">{formatDate(date)}</span>
+	</hgroup>
+
+	<div class="prose prose-slate dark:prose-invert">
+		<Content />
+	</div>
+</article>
