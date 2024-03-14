4e1dff5b297b4af54c46f78a8d98778a4e00de16
Raihan Adam <hallwack.id@gmail.com>
2024-03-14 22:53:22 +0700

    fix: add tailwindcss typography plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ import { fontFamily } from "tailwindcss/defaultTheme";
 const config = {
 	darkMode: ["class"],
 	content: ["./src/**/*.{html,js,svelte,ts}"],
-  safelist: ["dark"],
+	safelist: ["dark"],
 	theme: {
 		container: {
 			center: true,
@@ -59,6 +59,7 @@ const config = {
 			}
 		}
 	},
+	plugins: [require("@tailwindcss/typography")]
 };
 
 export default config;
